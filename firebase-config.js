f46ce89c5c0214c73488e9133bc4272bdb203463
jarvisbabbit-design <jarvisbabbit@gmail.com>
2026-07-09 23:13:31 +0200

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDgx3_AF1vDZpDQ6W5_hxhNGCxaJqcZ2YU",
-  authDomain: "turniej-do-ostatniego-tytana.firebaseapp.com",
-  databaseURL: "https://turniej-do-ostatniego-tytana-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "turniej-do-ostatniego-tytana",
-  storageBucket: "turniej-do-ostatniego-tytana.firebasestorage.app",
-  messagingSenderId: "657737249613",
-  appId: "1:657737249613:web:c64c9afeb41e898c4b3d04"
-};
